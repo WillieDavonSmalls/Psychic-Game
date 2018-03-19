a01c8d3da1d4207b67dec1cce7769dae8b406f4e
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2018-03-19 01:13:50 -0400

    updated code and increments,

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,6 @@
 //alphabet array
 var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-
-
 var letter = alpha[Math.floor(Math.random() * alpha.length)];
 
 //variables to track wins and losses
@@ -12,26 +10,37 @@ var lettersGuesed = [];
 var attemptCount = 10;
 
 
-for (i=0; i<10; i++){
-    document.onkeypress = function(event) {
-    var guess = event.key;
-        
-        attemptCount--;
+document.onkeypress = function(event) {
+		
+	var guess = event.key;
+	var resetGame = false;
+	
+	attemptCount--;
 
-        if (guess === letter){
-            winCount = winCount++;
-        } else{
-            lettersGuesed.push(guess);
-        }
-        if(lettersGuesed.length == 10){
-            lossCount++;
-        }
+	if(guess === letter){
+		//win and reset game
+		winCount++;
+		resetGame = true;
+	} else {
+		lettersGuesed.push(guess);
+    }
+	
+	if(lettersGuesed.length == 10){
+		//lose and reset game
+		lossCount++;
+		resetGame = true;
+    }
+    
+	//reset the game and reset the letter for the alphabet
+	if(resetGame) {
+		letter = alpha[Math.floor(Math.random() * alpha.length)];
+		lettersGuesed = [];
+		attemptCount = 10;
+
+	}
+        
     document.getElementById('numWins').innerHTML = 'Wins: ' + winCount;
     document.getElementById('numLosses').innerHTML = 'Losses: ' + lossCount;
     document.getElementById('numGuessesLeft').innerHTML = 'Guesses Left: ' + attemptCount;
     document.getElementById('lettersGuessed').innerHTML = 'Your Guesses So Far: ' + lettersGuesed.join();
-    }
-
-
-
-} 
+}
