77a55698330125351d5e778d19245805727ee75d
Willie Smalls <williesmalls@Willies-MacBook-Air.local>
2018-03-18 23:43:12 -0400

    additional work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,37 @@
 //alphabet array
 var alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-//variables to track wins and losses
-var wins = 0; 
-var losses = 0; 
-var attempts = 10; 
 
-//random letter selector
+
 var letter = alpha[Math.round(Math.random() * alpha.length)];
+
+//variables to track wins and losses
+var winCount = 0; 
+var lossCount = 0; 
+var attemptCount = 10; 
+var lettersGuesed = []
+
+for (i=0; i<attemptCount; i++){
+
+    document.onkeypress = function(event) {
+
+    
+        var guess = event.key;
+        if (guess === letter){
+            winCount = winCount++;
+        } else{
+            lettersGuesed.push(guess);
+        }
+        if(lettersGuesed.length == 10){
+            lossCount = lossCount++;
+        }
+    var attemptsLeft = attemptCount - lettersGuesed.length    
+    document.getElementById('numWins').innerHTML = 'Wins: ' + winCount;
+    document.getElementById('numLosses').innerHTML = 'Losses: ' + lossCount;
+    document.getElementById('numGuessesLeft').innerHTML = 'Guesses Left: ' + attemptsLeft;
+    document.getElementById('lettersGuessed').innerHTML = 'Your Guesses So Far: ' + lettersGuesed.join();
+    }
+
+
+
+} 
